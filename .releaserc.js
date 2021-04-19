b157f7db294a01a476ae485ea10773d28c04a49f
骆崇飞 <luochongfei@126.com>
2021-04-19 09:39:21 +0800

    ci: semantic-release 优化

---
+++ .releaserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    branch: "master",
+    preset: "angular",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        // "@semantic-release/npm",
+        [
+            "@semantic-release/git",
+            {
+                "assets": [
+                    "package.json",
+                    "CHANGELOG.md"
+                ],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
+        [
+            "@semantic-release/github",
+            {
+                "assets": "dist/*.tgz"
+            }
+        ]
+    ],
+}
